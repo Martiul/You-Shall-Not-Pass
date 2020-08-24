bdbaf627c6f8318e110f499c98e9944e0395d4dd
Martin <mkflui@uwaterloo.ca>
2020-08-24 13:31:18 -0400

    "Add thestar"

--- content.js
+++ content.js
@@ -1,17 +1,28 @@
-
-console.log(`[RUNNING] ${location.hostname}...`)
+// Match hostname with corresponding handler
 
 handlers = {
+    "www.bloomberg.com": bloomberg,
     "www.nytimes.com": nytimes,
     "www.washingtonpost.com": washingtonpost,
+    "www.thestar.com": thestar,
+}
+if (!location.hostname in handlers) {
+    console.log(`[WARNING] Could not find handler for ${location.hostname}`)
 }
 
+console.log(`[RUNNING] ${location.hostname}...`)
 waitForComplete = setInterval(() => {
     if (document.readyState != "complete") return
     handlers[location.hostname]()
     clearInterval(waitForComplete)
     console.log(`[DONE] ${location.hostname}`)
-}, 2000)
+}, 1000)
+
+function bloomberg() {
+    chrome.runtime.sendMessage({source: location.hostname})
+    // document.body.style.overflow="scroll"
+    // document.querySelector("#graphics-paywall-overlay").style.display="none"
+}
 
 function nytimes() {
     // TODO: likely dynamic class names
@@ -27,3 +38,13 @@ function washingtonpost() {
     document.querySelector(".paywall-overlay").style.display="none"
 }
 
+function thestar() {
+    document.querySelector(".basic-paywall-new").remove()
+    content = document.querySelector(".c-article-body__content").children
+    for (i = 0; i < content.length; i++) {
+        if (content[i].style.display == "none") {
+            content[i].style.display = ""
+        }
+    }
+
+}
